5c990b2024133f2965ae8e19418db55a3388a780
Jienan Zhang <zjn0505@hotmail.com>
2018-05-28 14:27:51 +0800

    Add omitted method

--- chrome-extension/script/main.js
+++ chrome-extension/script/main.js
@@ -210,3 +210,18 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+function getCurrentTabUrl(callback) {
+  var queryInfo = {
+    active: true,
+    currentWindow: true
+  };
+
+  chrome.tabs.query(queryInfo, function(tabs) {
+    var tab = tabs[0];
+    var url = tab.url;
+    console.assert(typeof url == 'string', 'tab.url should be a string');
+
+    callback(url);
+  });
+}
